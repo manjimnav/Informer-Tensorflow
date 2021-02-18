Fixed gradient update in ProbSparseAttention and added checkpoint directory creation

exp/exp_informer.py
@@ -74,7 +74,11 @@ class ExpInformer():
 
         train_steps = len(train_data)
         valid_steps = len(valid_data)
-        early_stopping = tf.keras.callbacks.EarlyStopping(patience=self.args.patience)
+        early_stopping = tf.keras.callbacks.EarlyStopping(patience=self.args.patience, restore_best_weights=True)
+
+        checkpoint_path = f'./checkpoints/{setting}'
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path)
 
         model_optim = self._select_optimizer()
         loss = tf.keras.losses.mse
@@ -89,7 +93,7 @@ class ExpInformer():
                        callbacks=[early_stopping],
                        epochs=self.args.train_epochs)
 
-        self.model.save_weights('../checkpoints/model.h5')
+        self.model.save_weights(checkpoint_path+'/model.h5')
 
         return self.model
 

models/attn.py
@@ -96,9 +96,12 @@ class ProbAttention(Layer):
             scores = (scores * attn_mask.mask) + (-((attn_mask.mask * num + num) - num))
 
         attn = tf.keras.activations.softmax(scores, axis=-1)  # nn.Softmax(dim=-1)(scores)
+        batch_indexes = tf.tile(tf.range(V.shape[0])[:, tf.newaxis, tf.newaxis], (1, V.shape[1], index.shape[-1]))
+        head_indexes = tf.tile(tf.range(V.shape[1])[tf.newaxis, :, tf.newaxis], (V.shape[0], 1, index.shape[-1]))
 
-        context_in = context_in.numpy()
-        context_in[np.arange(B)[:, None, None], np.arange(H)[None, :, None], index, :] = tf.matmul(attn, V).numpy()
+        idx = tf.stack(values=[batch_indexes, head_indexes, index], axis=-1)
+
+        context_in = tf.tensor_scatter_nd_update(context_in, idx, tf.matmul(attn, V))
 
         return tf.convert_to_tensor(context_in)
 